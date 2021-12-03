0e0b9a2e4278faea40a817bcaa6b3d3dc9642966
Casey <clabrack@gmail.com>
2021-12-03 16:17:18 -0500

    nicer solution

--- day3/part2.lua
+++ day3/part2.lua
@@ -5,59 +5,31 @@ end
 
 bits = #nums[1] -- number of bits in pattern
 
--- oxygen get
-oxynums = {}
-table.move(nums, 1, #nums, 1, oxynums) -- oxynums is clone of nums
-for i=1,bits do -- for each bit
-  local oxyfilter = ""
-  local ones = 0
-  for _, num in ipairs(oxynums) do -- in each number
-    if string.sub(num,i,i) == "1" then ones = ones + 1 end
-  end
-  if ones >= #oxynums/2 then
-    oxyfilter = "1"
-  else
-    oxyfilter = "0"
-  end
-
-  temp = {}
-  for _, num in ipairs(oxynums) do
-    if string.sub(num,i,i) == oxyfilter then
-      table.insert(temp, num)
+function rating (criteria)
+  ratingnums = {}
+  table.move(nums, 1, #nums, 1, ratingnums) -- clone of nums
+  for i=1,bits do -- for each bit
+    local filter = ""
+    local ones = 0
+    for _, num in ipairs(ratingnums) do -- count ones in each number
+      if string.sub(num,i,i) == "1" then ones = ones + 1 end
     end
+
+    filter = criteria[ones >= #ratingnums/2] -- true and false keyed to whatever type of rating critera
+
+    temp = {}
+    for _, num in ipairs(ratingnums) do
+      if string.sub(num,i,i) == filter then
+        table.insert(temp, num)
+      end
+    end
+    ratingnums = temp
+    if #ratingnums == 1 then break end
   end
-  oxynums = temp
-  if #temp == 1 then break end
+  return tonumber(ratingnums[1], 2)
 end
 
--- CO2 get
-co2nums = {}
-table.move(nums, 1, #nums, 1, co2nums) -- co2nums is clone of nums
-for i=1,bits do -- for each bit
-  local co2filter = ""
-  local ones = 0
-  for _, num in ipairs(co2nums) do -- in each number
-    if string.sub(num,i,i) == "1" then ones = ones + 1 end
-  end
-  if ones >= #co2nums/2 then
-    co2filter = "0"
-  else
-    co2filter = "1"
-  end
-
-  temp = {}
-  for _, num in ipairs(co2nums) do
-    if string.sub(num,i,i) == co2filter then
-      table.insert(temp, num)
-    end
-  end
-  co2nums = temp
-  if #temp == 1 then break end
-end
-
-oxy = tonumber(oxynums[1],2)
-co2 = tonumber(co2nums[1],2)
-
-print(string.format("oxygen: %d", oxy))
-print(string.format("co2: %d", co2))
-print(string.format("multiplied: %d", oxy * co2))
+-- table keys can anything in Lua, even true and false
+oxy = rating({[true] = "1", [false] = "0"})
+co2 = rating({[true] = "0", [false] = "1"})
+print(string.format("solution: %d", oxy * co2))
