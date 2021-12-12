c0c380e9471939ba9fe8d00aaf6af6b770a0e6c3
Casey <clabrack@gmail.com>
2021-12-12 15:25:50 -0500

    finall a day 6 part 2 solution

--- day6/part2.lua
+++ day6/part2.lua
@@ -2,50 +2,30 @@ f = assert(io.open("input.txt", "r"))
 data = f:read("a")
 f:close()
 
+-- an array to track how many fish there are at each age
 fishes = {}
-for num in string.gmatch(data, "%d+") do
-  table.insert(fishes, num)
+for i=0,8 do
+  fishes[i] = 0
 end
 
-count = 0
-
-
-function spawn (fish, generation)
-  if generation + fish <= 256 then
-    count = count + 1
-    spawn(9, generation + fish)
-    spawn(7, generation + fish)
-  end
+-- count number of fish at each age
+for num in string.gmatch(data, "%d") do
+  local n = tonumber(num)
+  fishes[n] = fishes[n] + 1
 end
 
-
-for _, fish in ipairs(fishes) do
-  spawn(fish, 1)
+-- every day, every fish's age decrements.
+-- age 0 fish are breeders. they become age 6
+-- and they create all of the current age 8 fish (newborns)
+for i=1,256 do
+  local breeders = fishes[0]
+  table.move(fishes,1,8,0) -- shift each element down one.
+  fishes[6] = fishes[6] + breeders
+  fishes[8] = breeders
 end
 
-
-
--- for i=1,80 do
---   for j=1,#fishes do
---     fishes[j] = fishes[j] - 1
---   end
---   local spawns = 0
---   for k=1,#fishes do
---     if fishes[k] == -1 then
---       fishes[k] = 6
---       spawns = spawns + 1
---     end
---   end
---   for l=1,spawns do
---     table.insert(fishes, 8)
---   end
---
---   --debug
---   -- io.write("After " .. i .. " days: ")
---   -- for _, fish in ipairs(fishes) do
---   --   io.write(" " .. fish)
---   -- end
---   -- io.write("\n")
--- end
-
-print(count + #fishes)
+sum = 0
+for idx, fish in pairs(fishes) do
+  sum = sum + fish
+end
+print(sum)
